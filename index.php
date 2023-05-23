9cab2d2e03d0f4d18b5298091c80faf09d541492
GiacomoArena <giacomoarena28@gmail.com>
2023-05-23 19:13:22 +0200

    insert css

--- index.php
+++ index.php
@@ -22,6 +22,12 @@ $arrayFish=[
   new fish('Aquaart Discovery Line Led Antracite', '€139,50', '<i class="fa-solid fa-fish"></i>','img\acquario.webp'),
 ];
   
+$arrayBird=[
+  new bird('Avena Decorticata', '€1,65', '<i class="fa-solid fa-dove"></i>','img\mangime-uccelli.webp'),
+  new bird('Voliera Wilma in Legno', '€270,30', '<i class="fa-solid fa-dove"></i>','img\gabbia.webp'),
+  new bird('Lettiera per Uccelli Ocean Fresh Air 5kg', '€2,90', '<i class="fa-solid fa-dove"></i>','img\lettiera-uccelli.webp'),
+];
+  
   //var_dump($arrayDogs);
 ?>
 
@@ -31,6 +37,7 @@ $arrayFish=[
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
   <!-- bootstrap -->
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
   <!-- fontawesome -->
@@ -41,10 +48,12 @@ $arrayFish=[
   <div class="container p-5">
     <h1 class="mt-5">Pet Shop</h1>
     
-    <h3 class="mt-5">Prodotti per Cani:</h3>
-    <section class="d-flex">
+    <div class="product">
+      <h3 class="mt-5">Prodotti per Cani:</h3>
+    </div>
+    <section class="d-flex justify-content-center">
       <?php foreach ($arrayDogs as $dog ): ?>
-        <div class="card mt-5 ms-3" style="width: 18rem;">
+        <div class="card my-5 ms-3" style="width: 18rem;">
           <img src="<?php echo $dog -> getImage() ?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?php echo $dog -> getDescription() ?></h5>
@@ -54,18 +63,21 @@ $arrayFish=[
             <h5 class="card-text">
               <?php echo $dog -> getPrice() ?>
             </h5>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
+            <a href="#" class="btn btn-warning">
+              Vai al carrello
             </a>
           </div>
         </div>
         <?php endforeach; ?>
     </section>
 
-    <h3 class="mt-5">Prodotti per Gatti:</h3>
-    <section class="d-flex">
+    <div class="product">
+      <h3 class="mt-5">Prodotti per Gatti:</h3>
+    </div>
+    
+    <section class="d-flex justify-content-center">
       <?php foreach ($arrayCats as $cat ): ?>
-        <div class="card mt-5 ms-3" style="width: 18rem;">
+        <div class="card my-5 ms-3" style="width: 18rem;">
           <img src="<?php echo $cat -> getImage() ?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?php echo $cat -> getDescription() ?></h5>
@@ -75,18 +87,21 @@ $arrayFish=[
             <h5 class="card-text">
               <?php echo $cat -> getPrice() ?>
             </h5>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
+            <a href="#" class="btn btn-warning">
+              Vai al carrello
             </a>
           </div>
         </div>
         <?php endforeach; ?>
     </section>
 
-    <h3 class="mt-5">Prodotti per Pesci:</h3>
+    <div class="product">
+      <h3 class="mt-5">Prodotti per Pesci:</h3>
+    </div>
+    
     <section class="d-flex">
       <?php foreach ($arrayFish as $fish ): ?>
-        <div class="card mt-5 ms-3" style="width: 18rem;">
+        <div class="card my-5 ms-3" style="width: 18rem;">
           <img src="<?php echo $fish -> getImage() ?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?php echo $fish -> getDescription() ?></h5>
@@ -96,8 +111,32 @@ $arrayFish=[
             <h5 class="card-text">
               <?php echo $fish -> getPrice() ?>
             </h5>
-            <a href="#" class="btn btn-primary">
-              Go somewhere
+            <a href="#" class="btn btn-warning">
+              Vai al carrello
+            </a>
+          </div>
+        </div>
+        <?php endforeach; ?>
+    </section>
+
+    <div class="product">
+      <h3 class="mt-5">Prodotti per uccelli:</h3>
+    </div>
+    
+    <section class="d-flex justify-content-center">
+      <?php foreach ($arrayBird as $bird ): ?>
+        <div class="card my-5 ms-3" style="width: 18rem;">
+          <img src="<?php echo $bird -> getImage() ?>" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo $bird -> getDescription() ?></h5>
+            <p class="card-text">
+              <?php echo $bird -> getcategories() ?>
+            </p>
+            <h5 class="card-text">
+              <?php echo $bird -> getPrice() ?>
+            </h5>
+            <a href="#" class="btn btn-warning">
+              Vai al carrello
             </a>
           </div>
         </div>
