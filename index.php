691751ba552b7e4d0940de98257c282c16d0b76f
GiacomoArena <giacomoarena28@gmail.com>
2023-05-23 17:10:33 +0200

    make the cards for all categories

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php
+  include_once __DIR__ . '/models/Bird.php';
+  include_once __DIR__ . '/models/Cat.php';
+  include_once __DIR__ . '/models/Dog.php';
+  include_once __DIR__ . '/models/Fish.php';
 
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +16,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- bootstrap -->
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <!-- fontawesome -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==' crossorigin='anonymous'/>
   <title>Pet Shop</title>
 </head>
 <body>
